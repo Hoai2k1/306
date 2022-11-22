19d9c1fcc30c49294a1154a9d06708c94293dbb9
hoai2k1 <hoaihoai488@gmail.com>
2022-11-22 19:46:53 +0700

    upload

---
+++ src/main/java/com/binh/service/impl/ServiceImpl_Role.java
@@ -0,0 +1,23 @@
+package com.binh.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+
+import com.binh.dao.DAO_Role;
+import com.binh.entity.Role;
+import com.binh.service.Service_Role;
+
+@SessionScope
+@Service
+public class ServiceImpl_Role implements Service_Role{
+
+	@Autowired private DAO_Role dao;
+
+	@Override
+	public List<Role> findAll() {
+		return dao.findAll();
+	}
+	
+}
